aa751dbd98c689b2bfe215e047a756cc7c9aacb4
Florent Poinsaut <florent@poinsaut.fr>
2023-12-04 10:41:02 +0100

    Fix storage permission to run as bareos user

--- storage/21-alpine/docker-entrypoint.sh
+++ storage/21-alpine/docker-entrypoint.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env ash
 
-bareos_sd_config="/etc/bareos/bareos-sd.d/director/bareos-dir.conf"
+bareos_sd_config_dir='/etc/bareos/bareos-sd.d'
+bareos_sd_config="${bareos_sd_config_dir}/director/bareos-dir.conf"
+control_file="${bareos_sd_config_dir}/bareos-config.control"
 
-if [ ! -f /etc/bareos/bareos-config.control ]; then
+if [ ! -f "${control_file}" ]; then
   tar xfz /bareos-sd.tgz --backup=simple --suffix=.before-control
 
   # Update bareos-storage configs
   sed -i 's#Password = .*#Password = '\""${BAREOS_SD_PASSWORD}"\"'#' $bareos_sd_config
 
   # Control file
-  touch /etc/bareos/bareos-config.control
+  touch "${control_file}"
 fi
 
 # Fix permissions
 find /var/lib/bareos ! -user bareos -exec chown bareos {} \;
-find /etc/bareos/bareos-sd.d ! -user bareos -exec chown bareos {} \;
+find "${bareos_sd_config_dir}" ! -user bareos -exec chown bareos {} \;
 find /dev -regex "/dev/[n]?st[0-9]+" ! -user bareos -exec chown bareos {} \;
 find /dev -regex "/dev/tape/.*" ! -user bareos -exec chown bareos {} \;
 
--- storage/nightly-ubuntu/docker-entrypoint.sh
+++ storage/nightly-ubuntu/docker-entrypoint.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-bareos_sd_config="/etc/bareos/bareos-sd.d/director/bareos-dir.conf"
+bareos_sd_config_dir='/etc/bareos/bareos-sd.d'
+bareos_sd_config="${bareos_sd_config_dir}/director/bareos-dir.conf"
+control_file="${bareos_sd_config_dir}/bareos-config.control"
 
-if [ ! -f /etc/bareos/bareos-config.control ]; then
+if [ ! -f "${control_file}" ]; then
   tar xfz /bareos-sd.tgz --backup=simple --suffix=.before-control
 
   # Update bareos-storage configs
   sed -i 's#Password = .*#Password = '\""${BAREOS_SD_PASSWORD}"\"'#' $bareos_sd_config
 
   # Control file
-  touch /etc/bareos/bareos-config.control
+  touch "${control_file}"
 fi
 
 # Fix permissions
-find /etc/bareos/bareos-sd.d ! -user bareos -exec chown bareos {} \;
+find "${bareos_sd_config_dir}" ! -user bareos -exec chown bareos {} \;
 chown -R bareos /var/lib/bareos /dev/[n]st* /dev/tape
 
 # Run Dockerfile CMD
